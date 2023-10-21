f0952acef3a32d759353d25728111f42be8adf4a
rhseung <rhseungg@gmail.com>
2023-10-21 11:21:05 +0900

    Create mapTriple.js

---
+++ src/functions/mapTriple.js
@@ -0,0 +1,12 @@
+/**
+ * [description]
+ *
+ * @example
+ * [example]
+ *
+ * @param {[type]} [description]
+ * @return {[type]} [description]
+ */
+module.exports = function mapTriple() {
+	// implement
+}
